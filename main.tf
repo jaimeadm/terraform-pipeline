a05c6dc9ff66391ae383268b39a44a5dd62c86cc
Jaime Adm <alan.silva@jaime.com.br>
2024-04-24 08:30:14 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,6 @@ terraform {
 provider "aws" {
   region = var.aws_region
   //profile = var.aws_profile
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  //access_key = var.aws_access_key
+  //secret_key = var.aws_secret_key
 }
