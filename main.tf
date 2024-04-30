3893854f4d9b1b690a55c40858b917dec3c43cd6
Jaime Adm <alan.silva@jaime.com.br>
2024-04-30 20:16:24 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 
 # Configure Provider AWS
 provider "aws" {
-  region = var.aws_region
+  # region = var.aws_region
   # profile = var.aws_profile
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
