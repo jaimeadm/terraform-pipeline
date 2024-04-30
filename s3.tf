123723056687023218da824e96ab3274ed8c1272
Jaime Adm <alan.silva@jaime.com.br>
2024-04-30 20:22:05 -0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,21 +1,21 @@
-resource "aws_s3_bucket" "b" {
-  bucket        = "terraform-test-100"
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "terraform-pipeline-jaime"
   force_destroy = true
   tags = {
-    Description = "Terraform Test 100"
-    Name        = "terraform-test-100"
+    Description = "Terraform Pipeline Jaime"
+    Name        = "terraform-pipeline-jaime"
   }
 }
 
-resource "aws_s3_bucket_versioning" "versioning_terraform-test" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "s3_bucket_public_terraform-test" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
