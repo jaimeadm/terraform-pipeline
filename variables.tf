31ac7bd8c80ed535a7cbe8d0d057166198ca08f9
Jaime Adm <alan.silva@jaime.com.br>
2024-04-30 20:20:19 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,4 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "aws_profile" {
-  type    = string
-  default = "default"
-}
-
-#variable "aws_access_key" {}
-#variable "aws_secret_key" {}
-
+# variable "aws_region" {}
+# variable "aws_profile" {}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
