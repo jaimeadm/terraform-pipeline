021ff7632cd9eebdab080ddd0dd71d314e63c448
Jaime Adm <alan.silva@jaime.com.br>
2024-04-30 20:27:22 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket Name"
-  value       = aws_s3_bucket.b.bucket
+  value       = aws_s3_bucket.bucket.bucket
 }
 
 output "bucket_arn" {
   description = "Bucket ARN"
-  value       = aws_s3_bucket.b.arn
+  value       = aws_s3_bucket.bucket.arn
 }
 
 output "bucket_tags" {
   description = "Bucket Tags"
-  value       = aws_s3_bucket.b.tags
+  value       = aws_s3_bucket.bucket.tags
 }
